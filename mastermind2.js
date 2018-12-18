ef5b9ca4521076eaccb1c6e992f8e481baa82360
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-12-18 09:31:03 -0500

    Still issues

--- mastermind2.js
+++ mastermind2.js
@@ -1,58 +1,36 @@
-/** Mastermind II - multiple turn @bmoreini*/
+/* Mastermind I - no saved turns */
+
+/* Initialization */
+//colors, code, guess, feedback -- all arrays
+var colors = ['r', 'c', 'w', 'g', 'b', 'y'];
+var code=[];
+var guess=[];
+var feedback=[];
+var cleanFeedback=[];
+var turnRecords=[];
+var turn=0;
+
 
 /* Main Method */
-function mainSequence() {
-	/* Initialization */
-	// colors, code, guess, feedback -- all arrays
-	// II: Add turnRecords Array (20), boolean gameOver
-	var colors = ['r', 'c', 'w', 'g', 'b', 'y'];
-	var code=[];
-	var guess=[];
-	var feedback=[];
-	var turnRecords=new Array(20);
-	var gameOver=false;
-	
-	/* Main Sequence */
-	turn=0;
-	turnRecords = populate(turnRecords);
-	code=setCode(code,colors);
+function main() {
+	code=setCode();
 	alert("Enter a color by keying the first letter of red, cyan, white, green, black, yellow.");
-	while (gameOver==false && turn<3) {
-		guess=getGuess(guess);
-		if (guess[1]=="q") {
-			gameOver=true;
-			break;
-		}
-		feedback=testGuess(guess,code,feedback);
-		if (feedback[3] == "b")  {
-			gameOver=true;
-			break;
-		}
-		addTurn("g",turnRecords,turn,guess);	
-		addTurn("f",turnRecords,turn,feedback);
-		showTurn(turnRecords,turn);
-		turnRecords=storeTurn(guess,feedback);
+	while (cleanFeedback[3]!="b" && guess[0]!="q"){
 		turn++;
-		}
-	alert("Charlie? My boy, you've won!");
+		guess=getGuess().slice(0);
+		feedback=testGuess(guess).slice(0);
+		cleanFeedback=formatFeedback(feedback).slice(0);
+		turnRecords=addTurn(turn, guess, cleanFeedback).slice(0);
+	}
+	if (cleanFeedback[3]=="b") alert("Charlie? You've won, my boy!");
+	else alert ("Quitter!");
 }
-
 
 /* Functions */
 
-/* Populate TurnRecords */
-function populate (turnRecords) {
-	for (var i = 0; i < 20; i++) {
-		turnRow = new Array(8);
-		turnRecords[i] = turnRow;
-	}
-	return turnRecords;
-}
-
-
-
 /* Create the Secret Code */
-function setCode(code,colors) {
+// function pulls from six colors to randomly fill code with four values
+function setCode() {
 	for (i=0;i<4;i++) {
 		code[i]=colors[Math.floor(Math.random()*6)];
 	}
@@ -61,107 +39,102 @@ function setCode(code,colors) {
 }
 
 /* Get a Player's Guess */
-function getGuess(guess) { 
-	for (i=0;i<4;i++) {	
-		guess[i]=prompt("Enter peg color "+(i+1)+":");
+// function prompts player for each of four values and stores in array
+function getGuess() {
+	for (i=0;i<4;i++) {
+		guess[i]=prompt("Enter color "+(i+1)+":");
 	}
 	console.log(guess);
 	return guess;
 }
 
 /* Analyze the Guess */
-function testGuess(guess,code,feedback) {
-	// create a temporary code
-	var tempGuess=guess;
-	var tempCode=code;
-	// count the blacks and erase guess and tempcode as you go
-	alert("Counting blacks....");
-	for (g=0;g<4;g++) {	
-		if (tempGuess[i]=="q") {
-			alert("What's wrong?");
-			gameOver=true;
-		}
-		if (tempGuess[g]!="" && guess[g]==tempCode[g]){
+function testGuess(guess) {
+	// function analyzes guess against code and produces feedback
+	// initialize the temporary code and guess arrays (copies)
+	var tempcode=code.slice(0);
+	var tempguess=guess.slice(0);
+	// count the blacks and erase tempcode as you go - one loop
+	for (g=0;g<4;g++) {
+		if (tempguess[g]!="" && tempguess[g]==tempcode[g]){
 			feedback[g]="b";
-			tempCode[g]="";	
-			tempGuess[g]="";
+			tempcode[g]="";
+			tempguess[g]="";
 		}
 	}
-	// count the whites and erase guess and tempcode as you go
-	debugger;
-	alert("Counting whites....");
-	for (g=0;g<4;g++) {	
+	console.log("Blacks Feedback: "+feedback);
+	// count the whites and erase tempcode as you go - nested loops
+	for (g=0;g<4;g++) {
 		for (c=0;c<4;c++) {
-			if (tempGuess[g]!="" && tempGuess[g]==tempCode[c]) {
-				feedback[g]="b";
-				tempCode[g]="";	
-				tempGuess[g]="";
+			if (tempguess[g]!="" && tempguess[g]==tempcode[c]) {
+				feedback[g]="w";
+				tempcode[c]="";
+				tempguess[g]="";
+				// use "continue" once a match is found in the inner loop
+				//continue;
 			}
 		}
 	}
 	// send the feeback to the formatter
-	debugger;
-	feedback=formatFeedback(guess,feedback);
-	// console log the feedback
-	console.log("Feedback: "+feedback);
+	console.log("Whites Feedback: "+feedback);
 	return feedback;
 }
 
+/* Format the Feedback */
 function formatFeedback(feedback) {
-	// initialize the black and white counts
-	var b=0, w=0;
+// function writes cleanFeedback to put b's first, w's second, delete the res
+	// initialize the black and white count variables
+	var b=0;
+	var w=0;
+	var cleanFeedback=[];
 	// count the blacks and whites
 	for (i=0;i<4;i++) {
+    	// count the black pegs
 		if (feedback[i]=="b") {
 			b++;
 		}
+    	// count the white pegs
 		else if (feedback[i]=="w") {
 			w++;
 		}
 	}
-	// delete the blanks after
+	// define remainder as 4 - (b + w)
 	var remainder = 4-(b+w);
 	// write the black pegs
 	for (i=0;i<b;i++) {
-		feedback[i]="b";
+		cleanFeedback[i]="b";
 	}
 	// write the white pegs
 	for (i=b;i<b+w;i++) {
-		feedback[i]="w";
+		cleanFeedback[i]="w";
 	}
-	// delete the remainder with array.pop()
-	for (i=0;i<remainder;i++) {
-		feedback.pop();
+	// delete the blanks in the feedback remainder with array.pop
+	for (i=1;i<remainder;i++) {
+		cleanFeedback.pop();
 	}
-	// return the new array
-	return feedback;
+	// return the new feedback array
+	console.log("Formatted Feedback: "+cleanFeedback);
+	return cleanFeedback;
 }
 
-/* Provide User Feedback */
-function showTurn(turnRecords,turn){
-	var turnRow=[];
-	for (i=0;i<turnRecords[turn].length;i++){
-		turnRow[i]=turnRecords[turn][i];
-		if (i==3) {
-			turnRow[i+1]=" :: ";
-			i++;
-		}
-	}
-	console.log(turnRow[turn]);
-}
 
 /* Add Turn (Guess, Feedback) to TurnRecords */
-function addTurn(type,turnRecords,turn,arrayValues){
-	if (type=="g") {
-		start=0;
-		stop=4;
+function addTurn(turn,guess,cleanFeedback){
+	var thisTurn =[];
+	var turnValues=4+cleanFeedback.length;
+	for (i=0;i<turnValues;i++){
+		if (i<4){
+			thisTurn.push(guess[i]);
+		}
+		else if (i>3){
+			thisTurn.push(cleanFeedback[i-4]);
+		}
 	}
-	else if (type=="f"){
-		start=4;
-		stop=arrayValues.length;
-	}
-	for (i=start;i<stop;i++) {
-		turnRecords[turn].splice(i,arrayValues[1]);	
-	}
-return turnRecords;
-}	
+	alert("This Turn = "+thisTurn);
+	turnRecords.push(thisTurn);
+	alert("Turn Records for turn "+turn+": "+turnRecords[turn-1]);
+	return turnRecords;
+	/*tested: No*/
+}
+
+
