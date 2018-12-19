e3a47e327947dcaf50966680bf0db03b294dc156
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-12-19 11:59:30 -0500

    Update and rename mastermind-class.js to mastermind-class-12-18.js

--- mastermind-class-12-18.js
+++ mastermind-class-12-18.js
@@ -1,4 +1,5 @@
 /* Class Mastermind */
+// NOTE: This file runs ONE turn only.  Test to ensure feedback is correct. 
 
 /* Initializations by Hasan*/
 // colors, code, guess, feedback (arrays) and turn (0);
@@ -9,22 +10,16 @@ colors = ["r","b","g","w","c","y"];
 
 /* Main Function */
 // define Main function
-	// set code = setCode(colors)
-	// start while loop - run while fourth feedback not = "b" and first guess not = "q"
-		// increment turn
-		// set guess = getGuess
-		// set feedback = testGuess(guess)
-		// alert turn and feedback
-	// }
-	// alert "Charlie you've won" if while loop ended with first condition
-	// alert "Quitter!" if while loop ended with section condition
-//}
-
 function main() {
-	turn++; // ADD IT
-	code=setCode(colors);  // FIX IT!
-	guess=getGuess(); // FIX IT!
-	feedback=testGuess(code,guess); // FIX IT!
+	// increment turn
+	turn++;
+	// set code = setCode(colors)
+	code=setCode(colors); 
+	// set guess = getGuess
+	guess=getGuess();
+	// set feedback = testGuess(guess)
+	feedback=testGuess(code,guess); 
+	// alert turn and feedback
 	alert("Guess "+turn+" : "+guess+" returns: "+feedback); // ADD IT
 }
 	
@@ -52,18 +47,13 @@ function getGuess(){
 
 /* Analyze the Guess */
 // define function testGuess to analyze guess against code and produces feedback
-// initialize b, w, as 0;
-// initialize tempCode and tempGuess arrays, as copies with array.slice(0);
-// count the blacks and erase tempcode and tempguess as you go - one loop
-// count the whites and erase tempcode and tempguess as you go - two nested loops
-	// use "continue" once a match is found in the inner loop
-// call the feedback function, sending it black and white counts
-// return the feedback
-
 function testGuess(code,guess){
+	// initialize b, w, as 0;
 	var b=0, w=0;
+	// initialize tempCode and tempGuess arrays, as copies with array.slice(0);
 	var tempCode = code.slice(0);
 	var tempGuess = guess.slice(0);
+	// count the blacks and erase tempcode and tempguess as you go - one loop
 	for (var g=0;g<4;g++){
 		if (tempGuess[g]==tempCode[g]) {
 			b++;
@@ -71,37 +61,41 @@ function testGuess(code,guess){
 			tempCode[g]="";
 		}
 	}	
+	// count the whites and erase tempcode and tempguess as you go - two nested loops
 	for (g=0;g<4;g++){
 		for (var c=0;c<4; c++){
 			if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
 				w++;
 				tempGuess[g]="";
 				tempCode[c]="";
+				// use "continue" once a match is found in the inner loop
 				continue;
 			}
 		}
 	}
+	// console.log the feedback
 	console.log("Blacks = "+b+" and Whites equals "+w);
+	// call the feedback function, sending it black and white counts
 	var feedback=formatFeedback(b,w);
+	// return the feedback
 	return feedback;
 }
 
-/* Format the Feedback */
+/* Format the Feedback */	
 // define function formatFeedback to rewrite the feedback array: put b's first, w's second
-	// clear the feedback
-	// write the black pegs first
-	// write the white pegs second
-	// console log the feedback
-	// return the feedback 
-	
 function formatFeedback(b,w){
+	// clear the feedback
 	var feedback=[]; 
+	// write the black pegs first
 	for (var i=0;i<b;i++){
 		feedback[i]="b";
 	}
+	// write the white pegs second
 	for (i=b;i<b+w;i++){
 		feedback[i]="w";
 	}
+	// console log the feedback
 	console.log("Feedback = "+feedback);
+	// return the feedback 
 	return feedback;
 }
