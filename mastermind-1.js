fc3587e2c28baa49abca401d7c5c0b71b143c1b3
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-04-19 19:29:37 -0400

    Update mastermind-1.js

--- mastermind-1.js
+++ mastermind-1.js
@@ -1,19 +1,15 @@
 /* Mastermind 1: Simple */
 
 /* Global variables or Globals */
-var colors = ["r","b","g","w","c","y"];
-var code = [];
-var guess = [];
-var feedback = [];
-var turn = 0;
+var colors = ["r","b","g","w","c","y"];code = []; guess = []; feedback = []; turn = 0
 
-/* Main Routine */ 
+/* Main */
 code=setCode(colors);
 while (feedback[3]!="b" && guess[0]!="q") {
 	turn++;
 	guess=getGuess(colors);
 	feedback=testGuess(code,guess);
-	console.log(feedback);
+	console.log("Turn "+turn+" guess: "+guess+" feedback: "+feedback);
 }
 if (feedback[3]=="b") alert("You guessed it in "+turn+" turns!");
 else if (guess[0]=="q") alert("Quitter!");
@@ -24,24 +20,19 @@ function setCode(colors){
   	for(var i=0; i<4; i++){
   		code[i]=colors[Math.floor(Math.random()*6)];
 	}
-	console.log(code);	
+	console.log(code);
   	return code;
 }
 
 function getGuess(colors){
-	for(var i=0; i<4; i++){
-		guess[i]=prompt("Enter a color [r]ed, [b]lack, [y]ellow, [g]reen, [c]yan, [w]hite"+(i+1));
-	}
-  	console.log(guess);
+	let guessString=prompt("Enter 4 colors, comma-separated: \n[r]ed, [b]lack, [y]ellow, [g]reen, [c]yan, [w]hite");
+	guess = guessString.split(",")
 	return guess;
 }
 
 function testGuess(code,guess){
-	// initialize b, w, as 0;
-	var b = 0, w = 0;
-	// initialize tempCode and tempGuess arrays, as copies with array.slice(0);
-	var tempCode = code.slice(0), tempGuess = guess.slice(0);
-	// count the blacks and erase tempCode and tempGuess as you go - one loop
+	let b = 0, w = 0;
+	let tempCode = code.slice(0), tempGuess = guess.slice(0);
 	for (g=0;g<4;g++){
 		if (tempGuess[g]==tempCode[g] && tempGuess[g]!=""){
 			b++;
@@ -49,40 +40,27 @@ function testGuess(code,guess){
 			tempCode[g]="";
 		}
 	}
-	// count the whites and erase tempCode and tempGuess as you go - nested loops
 	for (g=0;g<4;g++){
 		for (c=0;c<4;c++){
-        // use "continue" once a match is found in the inner loop
-		if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
-			w++;
-			tempCode[c]="";
-			tempGuess[g]="";
-			continue;
+			if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
+				w++;
+				tempCode[c]="";
+				tempGuess[g]="";
+				continue;
+			}
 		}
 	}
-}
-	// set feedback equal to called formatFeedback function, sending it black and white counts
-	// console.log("B = "+b+" and W = "+w);
 	feedback=formatFeedback(b,w);
-	// return the feedback
 	return feedback;
 }
 
-/* Format the Feedback */
-// define function formatFeedback to receive blacks and whites, put b's first, w's second, delete the rest
 function formatFeedback(b,w) {
-	// initialize feedback;
-	var feedback=[];
-	// loop from 0 to blacks count
+	let feedback=[];
 	for(var f=0;f<b;f++){
-		// write the black pegs first to feedback
 		feedback[f]="b";
 	}
-	// loop from blacks count to whites count
 	for(var f=b;f<w+b;f++){
-		// write the white pegs second to feedback
 		feedback[f]="w";
-	}	
-	// return the new feedback array
+	}
 	return feedback;
 }
