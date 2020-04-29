9557c28a389f6857a390850926653b9eade2bb11
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-04-29 10:42:26 -0400

    Update mastermind-1.js

--- mastermind-1.js
+++ mastermind-1.js
@@ -1,60 +1,51 @@
-/* Mastermind 1: Simple */
-
-/* Global variables or Globals */
-var colors = ["r","b","g","w","c","y"];code = []; guess = []; feedback = []; 
-
-/* Main Routine */
-code=setCode(colors);
-guess=getGuess(colors);
-feedback=testGuess(code,guess);
-console.log("Guess: "+guess+" Feedback: "+feedback);
-if (feedback[3]=="b") alert("You guessed it!");
-
-function setCode(colors){
-  	for(var i=0; i<4; i++){
-  		code[i]=colors[Math.floor(Math.random()*6)];
-	}
-	console.log(code);
-  	return code;
-}
-
-function getGuess(colors){
-	let guessString=prompt("Enter 4 colors, comma-separated: \n[r]ed, [b]lack, [y]ellow, [g]reen, [c]yan, [w]hite");
-	guess = guessString.split(",")
-	return guess;
-}
-
-function testGuess(code,guess){
-	let b = 0, w = 0;
-	let tempCode = code.slice(0), tempGuess = guess.slice(0);
-	for (g=0;g<4;g++){
-		if (tempGuess[g]==tempCode[g] && tempGuess[g]!=""){
-			b++;
-			tempGuess[g]="";
-			tempCode[g]="";
-		}
-	}
-	for (g=0;g<4;g++){
-		for (c=0;c<4;c++){
-			if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
-				w++;
-				tempCode[c]="";
-				tempGuess[g]="";
-				continue;
-			}
-		}
-	}
-	feedback=formatFeedback(b,w);
-	return feedback;
-}
-
-function formatFeedback(b,w) {
-	let feedback=[];
-	for(var f=0;f<b;f++){
-		feedback[f]="b";
-	}
-	for(var f=b;f<w+b;f++){
-		feedback[f]="w";
-	}
-	return feedback;
-}
+ /* mastermind hs */
+ main();
+ 
+ function main(){
+   var code = [], guess = [], colors = ["r", "y", "g", "c", "b", "w"], feedback=[], guessed=[];
+	 setUpCode();
+	 getGuess();
+	 analyzeGuess();
+	 console.log(guess, feedback);
+	 
+	 function setUpCode(){
+		 code = [];
+		 for(let peg=0;peg<4;peg++){
+			 let color=Math.floor(Math.random()*5)+1;
+			 code[peg]=colors[color];
+		 }
+     console.log(code);
+		 return code;
+	 }
+	 
+	 function getGuess(){
+		 guess=[];
+		 let guessString=prompt("Enter four colors for your guess (separated by commas, no spaces).");
+		 guess=guessString.split(",");
+		 return guess;
+	 }
+	 
+	 function analyzeGuess(){
+		 feedback = [];
+		 guessed = [];
+		 for(i=0;i<3;i++){
+			 if(guess[i]==code[i]){
+				 feedback.push("b");
+				 guessed.push(i);
+			 }
+      }
+		  for(g=0;g<3;g++){
+				 for(c=0;c<3;c++){
+					  if(guessed.includes(c)){
+						 continue;
+					  }
+						if(guess[g]==code[c]){
+							 feedback.push("w");
+							 guessed.push(c);
+							 continue;
+						}
+					}
+				}
+				 return feedback;
+		  }
+	  }
