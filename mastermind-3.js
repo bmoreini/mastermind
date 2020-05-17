4cf9d4764e2fcba788bb449fd187bd694db59f4f
bmoreini <bmoreini@bsdvt.org>
2020-05-17 16:32:39 +0000

    Updated MMIII

--- mastermind-3.js
+++ mastermind-3.js
@@ -1,163 +1,87 @@
-/* Class Mastermind */
-// NOTE: This file stores the guess and turns in thisTurn, and stores thisTurn in turnRecords.  
-// Test to ensure data is stored correctly.
+// ADD TRY CATCH FOR WHEN USER DOESNT GUESS AND EXITS
+// TypeError: Cannot read property 'split' of null
 
-/* Initializations */
-// colors, code, guess, feedback (arrays) and turn (0);
-// populate colors = r,b,g,w,c,y
-var colors=[], code=[], guess=[], feedback=[];
-var turn=0;
-colors = ["r","b","g","w","c","y"];
-// add arrays for thisTurn, turnRecords
-var thisTurn = [], turnRecords = [];
-var alertString="";
+var colors = ["r","y","g","b","w","c"];
+var turns = [];
+var feedback = [];
+var guess = [];
+var turn = [];
+var code = [];
 
-main();
-/* Main Function */
+code = setupCode();
+alert(code);
 
-// define Main function
-function main() {
-	// tell player the colors
-	alert("Colors include [r]ed, [c]yan, [y]ellow, [w]hite, [b]lack, [g]reen.");
-	// set code = setCode(colors)
-	code=setCode(colors);  
-	// we're going to loop as long as the answer is wrong and the player didn't want to quit, so:
-	// keep looping while fourth feedback is not a "b" and first guess is not a "q"
-	while (feedback[3]!="b" && guess[0]!="q")  {
-		// increment turn
-		turn++;
-		// set guess = getGuess
-		guess=getGuess(alertString);
-		// set feedback = testGuess(guess)
-		feedback=testGuess(code,guess); 
-    // store guess and feedback in thisTurn
-		thisTurn=addTurn(guess,feedback);
-		// push thisTurn to turnRecords
-		turnRecords.push(thisTurn);
-    // alert the guess and feedback for all turns (ugly display)
-		//alert("Turn and Feedback "+turnRecords);
-		alertString=formatTurnRecords(turnRecords);
-		//alert("Guess "+turn+" : "+guess+" returns: "+feedback); 
-	}
-	// alert "Charlie you've won" if while loop ended with first condition
-	if (feedback[3]=="b") {
-		alert ("Charlie, you've won!");
-	}
-	// alert "Quitter!" if while loop ended with section condition
-	else if (guess[0]=="q"){
-		alert("Quitter!");
-	}
+while(feedback[3]!="b" && guess[0]!="q"){
+  guess = getGuess();
+  feedback = analyzeGuess();
+  addTurn();
 }
-	
-/* Functions */
-
-/* Create the Secret Code */
-// define function setCode to pull from six colors to randomly fill code with four values 0-5
-function setCode(colors){
-	for(var i=0; i<4; i++){
-		code[i]=colors[Math.floor(Math.random()*6)];
-	}
-	console.log(code);
-	return code;
+if(turns[turns.length][1][3]="b"){
+  alert("Congrats! You guessed it in "+turns.length+"turns");
+}
+else{
+  alert("Quitter");
 }
 
-/* Get a Player's Guess */
-// define function getGuess to prompt player for each of four values and store in guess array
+function setupCode(){
+  code=[];
+  for(var peg=0;peg<4;peg++){
+    var color = Math.floor(Math.random()*6); // Arrays start at 0
+    code[peg] = colors[color];
+  }
+  console.log(code);
+  return code;
+}
+
 function getGuess(){
-	var myGuess=prompt(alertString+"\n Enter four colors surrounded by commas: ");
-	var guess = myGuess.split(',');
-	alert("Your guess was: "+guess);
-	return guess;
+  var guessString = prompt(displayTurns());
+  guess = guessString.split(",");
+  return guess;
 }
 
-/* Analyze the Guess */
-// define function testGuess to analyze guess against code and produces feedback
-function testGuess(code,guess){
-	// initialize b, w, as 0;
-	var b=0, w=0;
-	// initialize tempCode and tempGuess arrays, as copies with array.slice(0);
-	var tempCode = code.slice(0);
-	var tempGuess = guess.slice(0);
-	// count the blacks and erase tempcode and tempguess as you go - one loop
-	for (var g=0;g<4;g++){
-		if (tempGuess[g]==tempCode[g]) {
-			b++;
-			tempGuess[g]="";
-			tempCode[g]="";
-		}
-	}	
-	// count the whites and erase tempcode and tempguess as you go - two nested loops
-	for (g=0;g<4;g++){
-		for (var c=0;c<4; c++){
-			if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
-				w++;
-				tempGuess[g]="";
-				tempCode[c]="";
-				// use "continue" once a match is found in the inner loop
-				continue;
-			}
-		}
-	}
-	// console.log the feedback
-	console.log("Blacks = "+b+" and Whites equals "+w);
-	// call the feedback function, sending it black and white counts
-	var feedback=formatFeedback(b,w);
-	// return the feedback
-	return feedback;
+function analyzeGuess(){
+  var feedback = [];
+  var guessedCode = [];
+  var goodGuesses = [];
+
+  for(i=0;i<4;i++){
+    if(guess[i]==code[i]){
+      feedback += "b";
+      guessedCode += i;
+      goodGuesses += i;
+    }
+  }
+  for(g=0;g<4;g++){
+    if(goodGuesses.includes(g)==false){
+      for(c=0;c<3;c++){
+        if(guessedCode.includes(c)==false){
+          if(guess[g]==code[c]){
+            feedback+="w";
+            guessedCode+=c;
+            goodGuesses+=g;
+            break;
+          }
+        }
+      }
+    }
+  }
+  return feedback;
 }
 
-function formatFeedback(b,w){
-	var feedback=[]; 
-	for (var i=0;i<b;i++){
-		feedback[i]="b";
-	}
-	for (i=b;i<b+w;i++){
-		feedback[i]="w";
-	}
-	// console.log("Feedback = "+feedback);
-	return feedback;
+function addTurn(){
+  var turn = [];
+  turn.push(guess);
+  turn.push(feedback);
+  turns.push(turn);
 }
 
-/* Define function addTurn to make an array thisTurn from Guess and Feedback */
-function addTurn(guess,feedback){
-	// initialize thisTurn;
-	var thisTurn=[];
-	// set turnValues = 4 + length of feedback
-	var turnValues=4+feedback.length;
-	for (var i=0;i<turnValues;i++) {
-		// if index 0 - 3, write guess sub index
-		if (i < 4) {
-			thisTurn[i]=guess[i];
-		} 
-		// if index > 3, write feedback sub index-4 to correct for position
-		if (i > 3) {
-			thisTurn[i]=feedback[i-4];
-		}
-	} // end loop	
-	// console log thisTurn
-	console.log("thisTurn = "+thisTurn);
-  // NOTE: Once you get thisTurn console logged, comment out ALL OTHER console.logs except the code
-	// return thisTurn
-	return thisTurn;
-} // end function	
-
-/* Function to format turnRecords */
-// var text1 = "Hello";
-// var text2 = "World";
-// var text3 = text1.concat(" ", text2);
-
-function formatTurnRecords(turnRecords){
-	var alertString = "";
-	var thisGuess = "";
-	var thisFeedback = "";
-	for (var row=0;row<turn;row++) {
-		alertString = alertString.concat("Guess "+(row+1)+" : ");
-		thisGuess=turnRecords[row].slice(0, 4).join(" ");
-		alertString = alertString.concat(thisGuess);
-		alertString = alertString.concat(" || ");
-		thisFeedback= turnRecords[row].slice(4, turnRecords[row].length).join();
-		alertString = alertString.concat(thisFeedback);
-		alertString = alertString.concat("\n");
-	}
-	return alertString;
+function displayTurns(){
+  var mmPrompt = "";
+  for(var turnNumber=0;turnNumber<turns.length;turnNumber++){
+    mmPrompt += "Guess: " + (turnNumber+1) + " ";
+    mmPrompt += turns[turnNumber][0];
+    mmPrompt += " || " + turns[turnNumber][1];
+    mmPrompt += "\n";
+  }
+  return mmPrompt;
 }
